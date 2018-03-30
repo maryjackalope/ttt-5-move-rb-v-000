e42332b4424332b7a2e521b850a62c3cb021c556
Mary Jack <maryjackinthebox@gmail.com>
2018-03-30 21:44:22 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -3,5 +3,5 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 puts "Where would you like to go?"
 input = gets.strip
 index = input_to_index(input)
-move(board, index)
+move(board, 2)
 display_board(board)
