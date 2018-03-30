8d82b172e7af05cf8066d900d47ce39a9b5c82af
Mary Jack <maryjackinthebox@gmail.com>
2018-03-30 21:43:30 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,18 +1,7 @@
-
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
-  puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
-end
-
-def input_to_index(user_input)
-  user_input.to_i - 1
-end
-
-def move(board,index, player)
-  board[index] = player
-end
-
-
+puts "Welcome to Tic Tac Toe!"
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+puts "Where would you like to go?"
+input = gets.strip
+index = input_to_index(input)
+move(board, index)
+display_board(board)
